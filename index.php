685a070517c2a1934343a0019a0d60e7cd3b5285
Berend Wouters <online@visionsandviews.net>
2013-12-21 20:47:30 +0100

    Adds index.php
    The real deal

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+$spreadsheet_url="https://docs.google.com/spreadsheet/pub?key=0Api8y3Iuwmc6dHhLUHhnX0o4WXllTG41TDhqWWREemc&single=true&gid=1&output=csv";
+
+if(!ini_set('default_socket_timeout',    15)) echo "<!-- unable to change socket timeout -->";
+
+if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $spreadsheet_data[]=$data;
+        }
+    
+    fclose($handle);
+    // print_r($spreadsheet_data);
+    $firstElement = true;
+    foreach ($spreadsheet_data as $spreadsheet) {
+      if($firstElement){
+        $firstElement = false;
+      }else{
+        $gemeentes[] = $spreadsheet[1];
+      }
+      
+    }
+}
+else
+    die("Problem reading csv");
+?>
+<html>
+  <head>
+    <script type='text/javascript' src='https://www.google.com/jsapi'></script>
+    <script type='text/javascript'>
+     google.load('visualization', '1', {'packages': ['geochart']});
+     google.setOnLoadCallback(drawRegionsMap);
+
+      function drawRegionsMap() {
+        var data = google.visualization.arrayToDataTable([
+        ['City'],
+        <?php
+        foreach ($gemeentes as $gemeente) {
+          echo "['".$gemeente."'],";
+        }
+        ?>
+        ]);
+
+        var options = {
+          region: 'BE',
+          displayMode: 'markers',
+          colorAxis: {colors: ['green', 'blue']}
+        };
+
+        var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+    };
+    </script>
+  </head>
+  <body>
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
